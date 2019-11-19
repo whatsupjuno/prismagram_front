587e53b56eb1d93e8583108642e4434e92750961
Juno Bang <juno@coworksys.com>
2019-11-19 18:02:48 +0900

    header/meQuery problem

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -6,5 +6,16 @@ export default new ApolloClient({
   clientState: {
     defaults,
     resolvers
+  },
+  // request: async authorization => {
+  //   const token = await localStorage.getItem("token");
+  //   authorization.setContext({
+  //     headers: {
+  //       Authorization: token ? `Bearer ${token}` : ""
+  //     }
+  //   });
+  // }
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`
   }
 });
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+import gql from "graphql-tag";
 import styled, { ThemeProvider } from "styled-components";
+import { HashRouter as Router } from "react-router-dom";
 import { useQuery } from "react-apollo-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
-import Router from "./Router";
-import gql from "graphql-tag";
+import Routes from "./Routes";
 import Footer from "./Footer";
+import Header from "./Header";
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 935px;
+  max-width: ${props => props.theme.maxWidth};
   width: 100%;
 `;
 
@@ -29,12 +32,19 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <Wrapper>
+      <>
         <GlobalStyles />
-        <Router isLoggedIn={isLoggedIn} />
-        <Footer />
+        <Router>
+          <>
+            <Header />
+            <Wrapper>
+              <Routes isLoggedIn={isLoggedIn} />
+              <Footer />
+            </Wrapper>
+          </>
+        </Router>
         <ToastContainer position={toast.POSITION.TOP_RIGHT} />
-      </Wrapper>
+      </>
     </ThemeProvider>
   );
 };
