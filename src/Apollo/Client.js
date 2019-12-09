023a0e51cb5e3bc1cf69f92c3e6c9dc77659ac0f
Juno Bang <juno@coworksys.com>
2019-12-09 20:25:58 +0900

    real

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,6 +1,8 @@
 import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
+//changed
+
 export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
