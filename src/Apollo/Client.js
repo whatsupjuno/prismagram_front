6563ba155483052203cef5b6bfdb8268d9935a16
Juno Bang <juno@coworksys.com>
2019-12-10 04:42:35 +0900

    real

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -7,7 +7,7 @@ export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000"
-      : "https://prismagram-backend.herokuapp.com/",
+      : "https://junosgram-backend.herokuapp.com/",
   clientState: {
     defaults,
     resolvers
